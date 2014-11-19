db1721a8bc813b2e67869a939015db57ca7c86dc
elhorizonte <webmaster@elhorizonte.mx>
2014-11-19 11:54:28 -0600

    Update general_publicity.js

--- general_publicity.js
+++ general_publicity.js
@@ -11,8 +11,10 @@ function muestra_lateral_izquierdo(idZone){
 		_html += OA_output[idZone];
 		_html += '	</div>';
 		_html += '</div>';
-		$("#bannerlateralfixed_eu_izq").remove();
-		$(".footer").before(_html);
+		$().ready(function(){
+			$("#bannerlateralfixed_eu_izq").remove();
+			$(".footer").before(_html);
+		})
 	}
 }
 
@@ -26,7 +28,9 @@ function muestra_lateral_derecho(idZone){
 		_html += OA_output[idZone];
 		_html += '	</div>';
 		_html += '</div>';
-		$("#bannerlateralfixed_eu_der").remove();		
-		$(".footer").before(_html);
+		$().ready(function(){
+			$("#bannerlateralfixed_eu_der").remove();		
+			$(".footer").before(_html);
+		});
 	}
 }
